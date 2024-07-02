97a87aa7dfa1d62d0dc60e4201a4abe38ec18cb6
Ottish <ottapittaluga@hotmail.com>
2024-07-02 09:58:51 +0200

    Update shop-the-look-style-it.php

--- shop-the-look-style-it.php
+++ shop-the-look-style-it.php
@@ -6,6 +6,7 @@ Version: 1.0.1
 Author: SHOPTHELOOK SRL
 */
 
+
 if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
@@ -18,4 +19,12 @@ function initialize_shop_the_look_style_it() {
     new Shop_The_Look_Style_It();
 }
 add_action('plugins_loaded', 'initialize_shop_the_look_style_it');
+
+// Add a link to the Shopthelook website
+function shop_the_look_style_it_plugin_action_links($links) {
+    $b2b_link = '<a href="https://b2b.shopthelook.it" target="_blank">Visit B2B Site</a>';
+    array_unshift($links, $b2b_link);
+    return $links;
+}
+add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'shop_the_look_style_it_plugin_action_links');
 ?>
