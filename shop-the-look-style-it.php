8cfe11bca658f7099fa15a048557d96d8713b45b
Ottish <ottapittaluga@hotmail.com>
2024-07-02 09:28:44 +0200

    Create shop-the-look-style-it.php

---
+++ shop-the-look-style-it.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: ShopTheLook - Style It for WooCommerce
+Description:  Shop The Look Stile it is a plugin to create and manage product looks for WooCommerce.
+Version: 1.0.0
+Author: SHOPTHELOOK SRL
+*/
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
+
+// Include necessary files
+include_once('includes/class-shop-the-look-style-it.php');
+?>
