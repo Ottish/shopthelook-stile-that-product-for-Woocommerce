a56f46cba39f1373c8fe24b3faa171d7ebfd7527
Ottish <ottapittaluga@hotmail.com>
2024-07-02 09:39:19 +0200

    Update shop-the-look-style-it.php

--- shop-the-look-style-it.php
+++ shop-the-look-style-it.php
@@ -5,11 +5,16 @@ Description:  Shop The Look Stile it is a plugin to create and manage product lo
 Version: 1.0.0
 Author: SHOPTHELOOK SRL
 */
-
 if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
 
 // Include necessary files
 include_once('includes/class-shop-the-look-style-it.php');
+
+// Initialize the plugin
+function initialize_shop_the_look_style_it() {
+    new Shop_The_Look_Style_It();
+}
+add_action('plugins_loaded', 'initialize_shop_the_look_style_it');
 ?>
