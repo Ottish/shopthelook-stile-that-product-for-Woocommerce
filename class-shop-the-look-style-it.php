a9f15cd639ed5dc89e31e3e9008f53ef13da5f7b
Ottish <ottapittaluga@hotmail.com>
2024-07-02 09:29:34 +0200

    Create class-shop-the-look-style-it.php

---
+++ class-shop-the-look-style-it.php
@@ -0,0 +1,56 @@
+<?php
+
+class Shop_The_Look_Style_It {
+    public function __construct() {
+        add_action('init', array($this, 'register_post_type'));
+        add_action('add_meta_boxes', array($this, 'add_meta_boxes'));
+        add_action('save_post', array($this, 'save_meta_boxes'));
+    }
+
+    public function register_post_type() {
+        register_post_type('look', array(
+            'labels' => array(
+                'name' => __('Looks'),
+                'singular_name' => __('Look'),
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'supports' => array('title', 'editor', 'thumbnail'),
+        ));
+    }
+
+    public function add_meta_boxes() {
+        add_meta_box('look_products', __('Look Products'), array($this, 'look_products_meta_box'), 'look', 'side', 'default');
+    }
+
+    public function look_products_meta_box($post) {
+        $products = wc_get_products(array('limit' => -1));
+        $selected_products = get_post_meta($post->ID, '_look_products', true) ?: array();
+        ?>
+        <div>
+            <ul>
+                <?php foreach ($products as $product): ?>
+                    <li>
+                        <label>
+                            <input type="checkbox" name="look_products[]" value="<?php echo $product->get_id(); ?>" <?php checked(in_array($product->get_id(), $selected_products)); ?>>
+                            <?php echo $product->get_name(); ?>
+                        </label>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+        <?php
+    }
+
+    public function save_meta_boxes($post_id) {
+        if (isset($_POST['look_products'])) {
+            $products = array_map('intval', $_POST['look_products']);
+            update_post_meta($post_id, '_look_products', $products);
+        } else {
+            delete_post_meta($post_id, '_look_products');
+        }
+    }
+}
+
+new Shop_The_Look_Style_It();
+?>
